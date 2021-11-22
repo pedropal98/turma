534ef2587b54334f1f7c6bbc22c95a2e1f489317
Gyozo Szalontay <gyozo.szalontay@gmail.com>
2021-11-22 15:18:57 +0100

    create DataBase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_140104) do
+ActiveRecord::Schema.define(version: 2021_11_22_140557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,9 @@ ActiveRecord::Schema.define(version: 2021_11_22_140104) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "name", null: false
     t.string "nick_name"
-    t.string "role"
+    t.string "role", null: false
     t.string "address"
     t.text "rider"
     t.text "details"
