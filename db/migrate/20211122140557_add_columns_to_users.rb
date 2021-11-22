86de1957295f355fef7cc2122e8e6f86134d976d
Gyozo Szalontay <gyozo.szalontay@gmail.com>
2021-11-22 15:15:32 +0100

    created columnsToUsers

--- db/migrate/20211122140557_add_columns_to_users.rb
+++ db/migrate/20211122140557_add_columns_to_users.rb
@@ -1,9 +1,8 @@
 class AddColumnsToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :first_name, :string
-    add_column :users, :last_name, :string
+    add_column :users, :name, :string, null: false
     add_column :users, :nick_name, :string
-    add_column :users, :role, :string
+    add_column :users, :role, :string, null: false
     add_column :users, :address, :string
     add_column :users, :rider, :text
     add_column :users, :details, :text
