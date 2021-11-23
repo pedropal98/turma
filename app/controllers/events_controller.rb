c50299476675e2a0c53011ab668470c8abf0cdd3
Gyozo Szalontay <gyozo.szalontay@gmail.com>
2021-11-23 17:37:24 +0100

    made actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
 
-    # @invitation = Invitation.new
+    @invitation = Invitation.new
   end
 
   def edit
--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,2 +1,42 @@
 class InvitationsController < ApplicationController
+  def index
+    @invitations = Invitation.where(event: params[:event_id])
+  end
+
+  def create
+    @invitation = Invitation.new(invitation_params)
+    @invitation.user = current_user
+    @invitation.event_id = params[:event_id]
+    if @invitation.save!
+      flash[:alert] = "invitation!"
+      redirect_to event_path(params[:event_id])
+    else
+      flash[:alert] = "invitation failed"
+    end
+  end
+
+  def accept
+
+  end
+
+  def decline
+
+  end
+
+  def update
+    @invitation = Invitation.find(params[:id])
+    if params[:approved] === "true"
+      approved = true;
+    else
+      approved = false;
+    end
+    @invitation.update(approved: approved)
+    redirect_to event_invitations_path(params[:event_id])
+  end
+
+  private
+
+  def invitation_params
+    params.require(:invitation).permit(:event_id)
+  end
 end
