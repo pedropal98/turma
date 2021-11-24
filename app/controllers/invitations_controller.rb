114bac324f07cf7511c9a605d5c3c0fe37a18220
Gyozo Szalontay <gyozo.szalontay@gmail.com>
2021-11-24 16:43:04 +0100

    add new/create_invitation

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,42 +1,34 @@
 class InvitationsController < ApplicationController
   def index
-    @invitations = Invitation.where(event: params[:event_id])
+    @invitations = Invitation.where(user: current_user)
   end
 
   def create
     @invitation = Invitation.new(invitation_params)
-    @invitation.user = current_user
     @invitation.event_id = params[:event_id]
+    @invitation.user = User.find_by(name: params[:invitation][:user_id])
     if @invitation.save!
-      flash[:alert] = "invitation!"
       redirect_to event_path(params[:event_id])
     else
       flash[:alert] = "invitation failed"
     end
   end
 
-  def accept
-
-  end
-
-  def decline
-
-  end
-
-  def update
+  def destroy
     @invitation = Invitation.find(params[:id])
-    if params[:approved] === "true"
-      approved = true;
-    else
-      approved = false;
-    end
-    @invitation.update(approved: approved)
-    redirect_to event_invitations_path(params[:event_id])
+    @invitation.destroy
+    redirect_to event_path
   end
+
+  # def accept
+  # end
+
+  # def decline
+  # end
 
   private
 
   def invitation_params
-    params.require(:invitation).permit(:event_id)
+    params.require(:invitation).permit(:user_id, :content)
   end
 end
