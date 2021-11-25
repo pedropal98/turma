791980dae07ac207013d7832e2fbe31d0ff3a173
Philipp Krink <krink.philipp@gmail.com>
2021-11-25 11:58:40 +0100

    added host exception for ngrok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'events/calender', to: 'events#index'
   resources :events do
     resources :invitations, only: [ :new, :create ]
     resources :messages, only: :create
