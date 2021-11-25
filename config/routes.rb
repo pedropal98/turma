e28bd835407934f4019ead8b2cee901a6566c5c5
Philipp Krink <krink.philipp@gmail.com>
2021-11-25 12:28:13 +0100

    Added simple_Calender GEM

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get 'events/calender', to: 'events#index'
   resources :events do
     resources :invitations, only: [ :new, :create ]
     resources :messages, only: :create
