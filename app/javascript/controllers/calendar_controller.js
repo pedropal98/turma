8b4fdf9fe28d61ebd0e3a81e54550ce77c6e77e8
Philipp Krink <krink.philipp@gmail.com>
2021-11-26 15:10:42 +0100

    Worked a bit on font size in claender

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   static targets = ['list', 'calendarr', 'open', 'close']
 
   openCalendar() {
-    console.log("hello");
     this.calendarrTarget.classList.remove("hidden");
     this.listTarget.classList.add("hidden");
     this.openTarget.classList.add("hidden");
