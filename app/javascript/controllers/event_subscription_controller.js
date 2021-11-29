7ebf6f9330af13292ad750b64d3f10b7b63624ad
or granot <orgranot91@gmail.com>
2021-11-29 17:33:51 +0100

    break time

--- app/javascript/controllers/event_subscription_controller.js
+++ app/javascript/controllers/event_subscription_controller.js
@@ -3,16 +3,14 @@ import consumer from "../channels/consumer";
 
 export default class extends Controller {
   static values = { eventId: Number }
-  static targets = ['form']
 
   connect() {
     this.channel = consumer.subscriptions.create(
       { channel: "EventChannel", id: this.eventIdValue },
-      { received: data => this.element.insertAdjacentHTML("beforeend", data) },
-      //  console.log(formTarget)
-
-
+      { received: data => this.element.insertAdjacentHTML("beforeend", data) }
+      // { received: data => console.log(data) }
     )
+
   }
 
   disconnect() {
