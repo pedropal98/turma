8a2731b58ec7792dc70cf58175e8edc826f7e980
or granot <orgranot91@gmail.com>
2021-11-30 17:53:03 +0100

    NOTIFICATION WORKKKKSSSS

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['tab', 'close', 'content'];
+  static targets = ['tab', 'close', 'content', 'formUrl'];
 
   open() {
     this.tabTarget.classList.add("open");
@@ -10,8 +10,16 @@ export default class extends Controller {
       this.contentTarget.classList.remove("hidden");
     }, 500);
 
-    // Add autofocus to foem
+    // Add autofocus to form
     // Scroll to last message
+    fetch(this.formUrlTarget.action, {
+      method: 'PATCH',
+      headers: { 'Accept': 'text/plain' },
+      body: {}
+    }).then(response => response.text())
+      .then((data) => {
+        console.log(data);
+      })
 
   }
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     resources :messages, only: [:create]
   end
 
+  patch '/events/:event_id/messages', to: "messages#update"
+
 end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
