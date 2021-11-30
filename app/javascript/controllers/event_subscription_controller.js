f2fcf8d96ede679412268a54696c9d74857d4af0
or granot <orgranot91@gmail.com>
2021-11-30 10:28:10 +0100

    show page margin-bottom

--- app/javascript/controllers/event_subscription_controller.js
+++ app/javascript/controllers/event_subscription_controller.js
@@ -9,6 +9,5 @@ export default class extends Controller {
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: data => console.log(data) }
     )
-    console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`);
   }
 }
