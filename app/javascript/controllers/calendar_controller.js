dec575c9644f38bf82a10c1f12fe1d5f10aa565d
or granot <orgranot91@gmail.com>
2021-12-01 12:23:58 +0100

    logo rotate

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-  static targets = ['list', 'calendarr', 'open', 'close']
+  static targets = ['list', 'calendarr', 'logo', 'add', 'toRootLogo']
   static values = {open: Boolean}
 
   connect() {
@@ -11,18 +11,13 @@ export default class extends Controller {
     }
   }
 
-  openCalendar() {
-    this.calendarrTarget.classList.remove("hidden");
-    this.listTarget.classList.add("hidden");
-    this.openTarget.classList.add("hidden");
-    this.closeTarget.classList.remove("hidden");
-    this.openTarget.classList.add("open");
+  toggleCalendar() {
+    this.calendarrTarget.classList.toggle("hidden");
+    this.listTarget.classList.toggle("hidden");
+    this.logoTarget.classList.toggle("open");
   }
 
-  closeCalendar() {
-    this.calendarrTarget.classList.add("hidden");
-    this.listTarget.classList.remove("hidden");
-    this.openTarget.classList.remove("hidden");
-    this.closeTarget.classList.add("hidden");
+  resize() {
+    this.toRootLogoTarget.classList.toggle("rezise")
   }
 }
