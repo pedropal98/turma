060ab178e146ae23ecd1953490eda43941eb2b68
or granot <orgranot91@gmail.com>
2021-12-02 13:08:38 +0100

    chatroom finished

--- app/javascript/controllers/chat_controller.js
+++ app/javascript/controllers/chat_controller.js
@@ -1,13 +1,15 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ['tab', 'close', 'content', 'formUrl'];
+  static targets = ['tab', 'close', 'content', 'formUrl', 'messages'];
+
 
   open() {
     this.tabTarget.classList.add("open");
     setTimeout(() => {
       this.closeTarget.classList.remove("hidden");
       this.contentTarget.classList.remove("hidden");
+      this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
     }, 300);
 
     // Add autofocus to form
