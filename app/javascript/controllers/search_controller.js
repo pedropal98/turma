cbf43ef636c74f309669c7e45d152f50bbf1f813
or granot <orgranot91@gmail.com>
2021-12-02 11:33:15 +0100

    searchbar hidden on mobile

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
 
-  static targets = ['search', 'open', 'close', 'submit', 'searchForm']
+  static targets = ['search', 'open', 'close', 'submit', 'searchForm', 'input']
   static values = { open: Boolean }
 
   openSearch() {
@@ -10,14 +10,21 @@ export default class extends Controller {
 
     if (!activeSearch) {
 
-      this.searchTarget.classList.add("open-bar");
-      this.openTarget.classList.add("active");
-      this.searchTarget.focus();
+      this.searchTarget.classList.remove("hidden");
+      setTimeout(() => {
+        this.searchTarget.classList.add("open-bar");
+        this.openTarget.classList.add("active");
+
+      }, 100);
+      this.inputTarget.focus();
     }
     else {
-      if (this.searchTarget.value == '') {
+      if (this.inputTarget.value == '') {
         this.searchTarget.classList.remove("open-bar");
         this.openTarget.classList.remove("active");
+        setTimeout(() => {
+          this.searchTarget.classList.add("hidden");
+        }, 100);
       }
       else {
         this.searchFormTarget.submit();
